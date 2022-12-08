64b37c79d40f374d7453d3b989766a33f202000c
George Chen <georgc@iqmetrix.com>
2022-12-08 15:31:52 -0600

    day 8 part 2 refactoring

--- day8/p2.fsx
+++ day8/p2.fsx
@@ -4,10 +4,8 @@ let inputs=IO.File.ReadAllLines("input.txt")
 
 let height = inputs |> Array.length
 let width = inputs[0].Length
-let paddingX = -1
 
-type Point = {x: int; y: int} 
-type Tree = {value: int; visible: int; position: Point}  
+type Tree = {value: int; visible: int; }  
 
 let rec scan  (acc: Tree[]) (line: Tree list) =
     match line with
@@ -23,7 +21,8 @@ let rec scan  (acc: Tree[]) (line: Tree list) =
             match x with
             | Some(i) -> i+1
             | None -> acc.Length
-        |> fun i -> scan [|{h with visible = h.visible * i}; yield! acc;  |] tail
+        |> fun i -> 
+            scan [|{h with visible = h.visible * i}; yield! acc;  |] tail
 
 let rotate (g: Tree[][]) =
     let width = g.[0].Length
@@ -31,22 +30,20 @@ let rotate (g: Tree[][]) =
 
     [| for i in 0..(width - 1) -> (width - 1 - i) |]
     |> Array.map(fun x ->
-        [|0..height-1|]
-        |> Array.map(fun y -> g.[y][x] ))
-    
-inputs
-|> Array.map(fun x -> x.ToCharArray() )
-|> Array.mapi(fun i x ->
-        x 
-        |> Array.mapi (fun j c -> {value =(int c) - (int '0'); visible = 1; position = {x = j; y = i}}))
-|> Array.map(fun line -> scan Array.empty (line |> List.ofArray))
-|> rotate
-|> Array.map(fun line -> scan Array.empty (line |> List.ofArray))
-|> rotate
-|> Array.map(fun line -> scan Array.empty (line |> List.ofArray))
-|> rotate
-|> Array.map(fun line -> scan Array.empty (line |> List.ofArray))
-|> rotate
+        [|0..height-1|] |> Array.map(fun y -> g.[y][x] ))
+        
+let trees =    
+    inputs
+    |> Array.map(fun x -> x.ToCharArray() )
+    |> Array.map(
+        Array.map (fun  c -> {value =(int c) - (int '0'); visible = 1; }))
+
+[1..4]
+|> List.fold(fun trees _ ->
+    trees
+        |> Array.map(fun line -> scan Array.empty (line |> List.ofArray))
+        |> rotate
+    ) trees
 |> Array.collect(fun line -> line)
 |> Array.maxBy(fun t -> t.visible)
 |> printfn " %A"
